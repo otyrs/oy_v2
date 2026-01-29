c35531d2436e9254b7fa210676c70b2469fe7006
loop4myheart <nasan.gb@gmail.com>
2026-01-29 20:48:48 +0900

    go

--- oy/src/main.jsx
+++ oy/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../index.css'
 import Layout from './Layout.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
--- oy/vite.config.js
+++ oy/vite.config.js
@@ -1,17 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   css: {
     transformer: "postcss",
     minify: "esbuild",
   },
-  // --- ここから追加 ---
-  preview: {
-    preview: {
-      allowedHosts: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
     },
   },
-  // --- ここまで ---
+  preview: {
+    allowedHosts: ["oy-v2.onrender.com"],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
