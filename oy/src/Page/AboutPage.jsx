bea374f77b4e42d13c16de74bb5d7d6d6622f987
loop4myheart <nasan.gb@gmail.com>
2026-02-07 22:48:37 +0900

    update

--- oy/src/Page/AboutPage.jsx
+++ oy/src/Page/AboutPage.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import { useEffect } from 'react';
 import { useArticlesStore } from '../libs/articleStore.js';
+
 
 export default function AboutPage() {
     const articles = useArticlesStore((state) => state.articles);
-    const fetchArticles = useArticlesStore((state) => state.fetchArticles);
 
     const aboutArticles = articles
         .filter(article => {
             if (!article.category) return false;
             const cats = Array.isArray(article.category) ? article.category : [article.category];
             return cats.includes("About");
-    });
+        });   
+  
+    const currentLanguage = useArticlesStore((state) => state.currentLanguage);
+  
+    const displayContent = currentLanguage === 'en' 
+        ? aboutArticles[0]?.content_en 
+        : aboutArticles[0]?.content;
 
-   
-    console.log("AboutPage articles", aboutArticles);
     return (
       
     <div>
           <h2 className="text-2xl m-auto text-center">About</h2>
-            <div className="mb-8 p-4" style={{ whiteSpace: "pre-line" }} dangerouslySetInnerHTML={{ __html: aboutArticles[0]?.content }}></div>
+            <div className="mb-8 p-4" style={{ whiteSpace: "pre-line" }} dangerouslySetInnerHTML={{ __html: displayContent }}></div>
     </div>
   )
 }
