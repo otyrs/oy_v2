16068461dd5f451b5dd6dc1f0d7cce69bbb1a5d0
loop4myheart <nasan.gb@gmail.com>
2026-05-13 17:44:55 +0900

    up

--- oy/src/components/Footer/Footer.jsx
+++ oy/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer w-full h-12 flex justify-center align-center items-center">
-      <div className='opacity-50'>© 2025 All rights reserved.</div>
+      <div className='opacity-50'>© 2026 All rights reserved.</div>
     </div>
   )
 }
