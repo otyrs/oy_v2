3b0e5c8b1568e91bb3faaeab7bfcf25dd6daa53a
loop4myheart <nasan.gb@gmail.com>
2026-05-15 21:34:40 +0900

    up

--- oy/src/Page/TopPage.jsx
+++ oy/src/Page/TopPage.jsx
@@ -4,7 +4,7 @@ export default function TopPage() {
   return (
     <div className='w-full h-full flex justify-center align-center items-center'>
       <img
-        src="./logo.png"
+        src="/logo.png"
         alt="Logo"
       className="w-50 h-50 mb-20"/>
     </div>
